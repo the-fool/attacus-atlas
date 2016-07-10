2fdaf38769c46aec748fb226da93bf8ebcf130d3
the-fool <sketchbang@gmail.com>
2016-07-10 13:56:51 -0500

    dynamically add post innerhtml

--- src/app/blog/blog.service.ts
+++ src/app/blog/blog.service.ts
@@ -4,12 +4,18 @@ import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class BlogService {
-  constructor(private http: Http) {}
-  private postingsUrl = '/assets/postings/index.json';
-
-  getPostings(): Observable<PostingMeta[]>  {
+  constructor(private http: Http) { }
+  private postingsUrl = '/assets/postings';
+  getPostings(): Observable<PostingMeta[]> {
     console.log('Get postings');
-    return this.http.get(this.postingsUrl)
-                    .map(res => res.json().postings);
+    return this.http
+      .get(`${this.postingsUrl}/index.json`)
+      .map(res => res.json().postings);
+  }
+
+  fetchPosting(dir: String): Observable<String> {
+    return this.http
+      .get(`${this.postingsUrl}/${dir}/post.html`)
+      .map(res => res.text())
   }
 }
--- src/app/blog/posting/posting.component.ts
+++ src/app/blog/posting/posting.component.ts
@@ -1,19 +1,33 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+import { BlogService } from '../blog.service';
+
 @Component({
   selector: 'posting',
   template: `
     <h3>A posting</h3>
-  `
+    <div [innerHtml]="innerHtml"></div>
+  `,
+  providers: [
+    BlogService
+  ]
 })
 export class Posting implements OnInit {
+  private innerHtml: String;
+
   constructor(
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private blogService: BlogService
   ) {}
 
   ngOnInit() {
-    console.log(this.route);
+    this.route.params.subscribe(params => {
+      let dir = params['dir'];
+      this.blogService.fetchPosting(dir).subscribe(res => {
+        this.innerHtml = res;
+      });
+    });
   }
 
 }
